bed62f187dc14d769904c765e58b88a43fb09a31
Sung Kim <jstkim7@gmail.com>
2013-12-28 08:06:29 -0500

    Create marketing pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 BookTracker::Application.routes.draw do
+  get "marketing_pages/home"
   root to: "books#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
