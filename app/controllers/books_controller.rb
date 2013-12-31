dbb014f079d719fcef7d880eef196fbd8ffb21ad
Sung Kim <jstkim7@gmail.com>
2013-12-31 15:38:15 -0500

    Gemfiles

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,4 +4,15 @@ class BooksController < ApplicationController
     @colors = %w( pink sand light-sand light-blue ).cycle
     @book = Book.new
   end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to root_path
+  end
+
+  private
+    def book_params
+      params.require(:book).permit(:title, :author, :description)
+    end
 end
