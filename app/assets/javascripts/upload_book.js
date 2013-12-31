6eda171ed44ac2bac777dd888da00bd2c18699d9
Sung Kim <jstkim7@gmail.com>
2013-12-30 19:30:17 -0500

    Partial popup book submit

--- app/assets/javascripts/upload_book.js
+++ app/assets/javascripts/upload_book.js
@@ -1,22 +1,25 @@
-var obj = $("#new-book");
-obj.on('dragenter', function (e)
-{
-    e.stopPropagation();
-    e.preventDefault();
-    $(this).css('border', '2px solid #0B85A1');
-});
-obj.on('dragover', function (e)
-{
-     e.stopPropagation();
-     e.preventDefault();
-});
-obj.on('drop', function (e)
-{
- 
-     $(this).css('border', '2px dotted #0B85A1');
-     e.preventDefault();
-     var files = e.originalEvent.dataTransfer.files;
- 
-     //We need to send dropped files to Server
-     handleFileUpload(files,obj);
+$(function() {
+  $( "#dialog-form" ).dialog({
+    autoOpen: false,
+    resizable: false,
+    width: 500,
+    modal: true,
+    show: {
+      effect: "blind",
+      duration: 500
+    },
+    hide: {
+      effect: "explode",
+      duraction: 500
+    },
+    buttons: {
+      Cancel: function() {
+        $( this ).dialog( "close" );
+      }
+    }
+  });
+
+  $( "#new-book" ).click(function() {
+    $( "#dialog-form" ).dialog( "open" );
+  });
 });
