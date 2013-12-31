1753d70a91e600754c160dd8322a6069c8bd48b8
Sung Kim <jstkim7@gmail.com>
2013-12-31 16:30:40 -0500

    Create paper clip methods

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,7 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
+    @book.document = params[:book][:document]
     @book.save
     redirect_to root_path
   end
