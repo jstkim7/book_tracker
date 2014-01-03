4c80178aaf9ab8f01793a1da034b8f58c289cbe4
Sung Kim <jstkim7@gmail.com>
2014-01-02 21:50:56 -0500

    Add separate title field

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,6 @@ class Book < ActiveRecord::Base
   validates :title, presence: true
 
   def uploaded_pdf=(pdf_field)
-    self.title        = base_part_of(pdf_field.original_filename)
     self.content_type = pdf_field.content_type.chomp
     self.data         = pdf_field.read
   end
